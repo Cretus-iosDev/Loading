e772c84ac41086e43897eff1d61044e6c33073fb
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2021-08-08 10:10:57 +0530

    Add files via upload

---
+++ Loading/Loading/LoadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingApp.swift
+//  Loading
+//
+//  Created by rutik maraskolhe on 08/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LoadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
